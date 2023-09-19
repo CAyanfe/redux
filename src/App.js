581c66d235d53531f422e7def18d0461a3eeb7a9
CAyanfe <abiodunfaith15@gmail.com>
2023-09-20 00:59:45 +0100

    redux

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// src/App.js
+import React from "react";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import ListTask from "./components/ListTask";
+import Filter from "./components/Filter";
+
+function App() {
+  return (
+    <div className="content-container">
+      <h1>ToDo Application</h1>
+      <div className="container">
+        <AddTask />
+        <ListTask />
+        {/*  <Filter /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
